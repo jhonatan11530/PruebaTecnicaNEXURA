6a7f7f197ef6980bf51c7d3d7e46f56ec6f4ef48
jhonatan fernandez <jhonatan1153@hotmail.com>
2023-05-16 21:07:27 -0500

    se crea archivo de configuracion de autoarrance

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (!File::exists(base_path('.env'))) {
+            File::copy(base_path('.env.example'), base_path('.env'));
+            Artisan::call('key:generate');
+        }
     }
 }
